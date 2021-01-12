6cc11af45899031ca7cb0acaae0d3a572b09a302
mhsnabz <mhsnabz@yahoo.com.tr>
2021-01-13 00:48:25 +0300

    ~

---
+++ VayeApp/app/src/main/java/com/vaye/app/Controller/HomeController/LessonPostEdit/EditPostActivity.java
@@ -0,0 +1,16 @@
+package com.vaye.app.Controller.HomeController.LessonPostEdit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vaye.app.R;
+
+public class EditPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_post);
+    }
+}
